e4c7081893d450392b02f37a33cfe90f4d3d34f7
vYareskoDev <vyareskodev@gmail.com>
2022-12-05 12:43:56 +0200

    some more updates

---
+++ src/CustomExceptions/FetchException.ts
@@ -0,0 +1,9 @@
+export default class FetchException extends Error
+{
+    constructor(message: string)
+    {
+        super(message)
+        this.name = "FetchException"
+    }
+
+}
--- src/Hooks/useFetch.ts
+++ src/Hooks/useFetch.ts
@@ -1,1 +1,23 @@
-export default null;
+import Task from "../Interfaces/Task";
+import { useEffect } from 'preact/hooks'
+import FetchException from "../CustomExceptions/FetchException";
+
+async function getResponce(tasksArr: Task[], reqUrl: string) {
+    const responce = await fetch(reqUrl)
+    if(!responce.ok)
+    {
+        throw new FetchException("Fetch error");
+    }
+    const data = (await responce.json())
+    return tasksArr;
+}
+
+async function useFetchTodos(reqUrl: string, error: FetchException, options?: RequestInit): Promise<Task[]>
+{
+    const tasks: Task[] = [];
+    useEffect(async (): Promise<Task[]> => (getResponce(tasks, reqUrl)), [reqUrl, options])
+    return tasks
+}
+
+
+export default useFetchTodos
